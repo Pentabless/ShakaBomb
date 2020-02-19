79d6fa1565322c3c137febb59a94e0ef2b8be01e
KubotaP <teapo6264@gmail.com>
2020-02-19 15:34:11 +0900

    切り替えし挙動の安定 カメラ追従をスムーズに

--- CESA-2020-Prototype/Assets/Scripts/CameraController.cs
+++ CESA-2020-Prototype/Assets/Scripts/CameraController.cs
@@ -20,6 +20,25 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //// プレイヤーのポジションとカメラのポジションを調べる
+        //Vector3 playerPos = player.transform.position;
+        //// ジャンプでは動かさない
+        //if (yRange <= Mathf.Abs(camera.transform.position.y) || yRange >= camera.transform.position.y)
+        //{
+        //    playerPos.y = camera.transform.position.y;
+        //}
+
+        //// プレイヤーに追従
+        //playerPos.z = camera.transform.position.z;
+        //camera.transform.position = Vector3.Lerp(camera.transform.position, playerPos, 0.05f);
+
+        //// デバック処理
+        //if (Input.GetKeyDown(KeyCode.A))
+        //    cameraShake.Shake(0.5f, 0.3f);
+    }
+
+    private void FixedUpdate()
+    {
         // プレイヤーのポジションとカメラのポジションを調べる
         Vector3 playerPos = player.transform.position;
         // ジャンプでは動かさない
@@ -34,6 +53,6 @@ public class CameraController : MonoBehaviour
 
         // デバック処理
         if (Input.GetKeyDown(KeyCode.A))
-            cameraShake.Shake(0.5f,0.3f);
+            cameraShake.Shake(0.5f, 0.3f);
     }
 }
