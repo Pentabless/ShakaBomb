177392acf84148c150cbdac40b1c1ba754dd9e43
PinkStarWarrior <kawaguchi.ami2021@gmail.com>
2020-02-27 17:12:03 +0900

    羊の敵(泡を飛ばす・泡がなくなると地面から落ちないように走り回る)

--- CESA-2020-Prototype/Assets/EnemyBubbleController.cs
+++ CESA-2020-Prototype/Assets/EnemyBubbleController.cs
@@ -10,36 +10,44 @@ public class EnemyBubbleController : MonoBehaviour
     Vector2 move_force;
     //円運動するための角度
     float angle;
-    //
-    Rigidbody2D rigid;
 
     // Start is called before the first frame update
     void Start()
     {
         isBlow = false;
-        move_force = new Vector2(0.025f, 0.025f);
-        angle = Random.Range(0.0f,180.0f);
-        rigid = GetComponent<Rigidbody2D>();
+        move_force = new Vector2(0.025f, 0.025f * Random.Range(0.1f, 3.0f));
+        angle = Random.Range(0.0f, 180.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
         //既に飛んでいたら
-        if(isBlow)
+        if (isBlow)
         {
             angle += 0.1f;
             //上下に揺れながら進む
-            transform.Translate(-move_force.x, Mathf.Sin(angle)*move_force.y, 0.0f);
+            transform.Translate(move_force.x, Mathf.Sin(angle) * move_force.y, 0.0f);
+
+            //画面外に出たら
+            if(!GetComponent<Renderer>().isVisible)
+            {
+                //消える
+                Destroy(this.gameObject);
+            }
         }
         else
         {
             //親がいなかったら
-            if(transform.parent==null)
+            if (transform.parent == null)
             {
                 isBlow = true;
             }
         }
     }
 
+    public void SetDir(int dir)
+    {
+        move_force.x *= dir;
+    } 
 }
--- CESA-2020-Prototype/Assets/EnemyController.cs
+++ CESA-2020-Prototype/Assets/EnemyController.cs
@@ -14,6 +14,14 @@ public class EnemyController : MonoBehaviour
     private const int attack_interval = 120;
     //飛ばす泡のカウント
     int attack_count;
+    //初期の向き
+    int start_dir;
+    //今の向き
+    int dir;
+    //逃げる速さ
+    float escape_vel;
+    //進む先に床がある
+    bool fall_checker;
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +43,14 @@ public class EnemyController : MonoBehaviour
         Debug.Log("NumAttack" + num_attacks);
 
         attack_count = 0;
+
+        //左向き…-1　右向き…1
+        start_dir = (int)(transform.localScale.x);
+        dir = start_dir;
+
+        escape_vel = 0.05f;
+
+        fall_checker = true;
     }
 
     // Update is called once per frame
@@ -46,6 +62,8 @@ public class EnemyController : MonoBehaviour
             //プレイヤーを見つけていて泡を飛ばすインターバルが終わったら
             if ((find_player == true) && (attack_count <= 0))
             {
+                //飛ばす泡の向きを設定する
+                enemy_attacks[num_attacks - 1].GetComponent<EnemyBubbleController>().SetDir(start_dir);
                 //後ろの要素から飛ばすようにする
                 enemy_attacks[num_attacks - 1].parent=null;
                 //泡の数を変更する
@@ -54,9 +72,31 @@ public class EnemyController : MonoBehaviour
                 attack_count = attack_interval;
             }
         }
+        //残ってなかったら
+        else if(num_attacks==0)
+        {
+            //逃げる方向を決める
+            dir *= -1;
+            //この処理に当たらないようにする
+            num_attacks--;
+        }
+        else
+        {
+            transform.Translate(dir * escape_vel, 0.0f, 0.0f);
+        }
+
+        //進む先に床がなかったら
+        if(fall_checker==false)
+        {
+            //方向を変える
+            dir *= -1;
+            //床がある事にする
+            fall_checker = true;
+        }
 
         //攻撃のカウントダウンをする
         attack_count--;
+        transform.localScale = new Vector3(dir, 1.0f, 1.0f);
     }
 
     public void FindPlayer(bool find)
@@ -70,5 +110,10 @@ public class EnemyController : MonoBehaviour
         {
             Debug.Log("プレイヤーどこ？");
         }
+    }
+
+    public void SetFallChecker(bool check)
+    {
+        fall_checker = check;
     }
 }
