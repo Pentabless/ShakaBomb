e5df49b301835092b7ae0135e7bad9f44677a3bf
T.Yanase <41750621+TakumiYanase@users.noreply.github.com>
2020-03-27 17:06:10 +0900

    ポーズメニューの追加

---
+++ CESA-2020-Prototype/Assets/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas m_menu = null;
+
+    private void Awake()
+    {
+        m_menu.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            m_menu.gameObject.SetActive(true);
+        }
+    }
+}
