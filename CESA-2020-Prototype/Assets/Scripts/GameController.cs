f89574abc9885b6e1ff445dc4cba84f0ff148072
KubotaP <teapo6264@gmail.com>
2020-04-01 10:03:26 +0900

    動作テスト兼Prefabの更新

--- CESA-2020-Prototype/Assets/Scripts/GameController.cs
+++ CESA-2020-Prototype/Assets/Scripts/GameController.cs
@@ -8,7 +8,7 @@ public class GameController : MonoBehaviour
     private bool m_checkGamepad = false;
 
     private void Awake()
-    {   
+    {
         // 接続されているゲームパッドの名前を調べる
         var gamepadNames = Input.GetJoystickNames();
 
