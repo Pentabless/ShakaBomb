fa6bcae7767653d8521dc9e1cd5a5f8201497c2a
KubotaP <teapo6264@gmail.com>
2020-04-17 11:41:22 +0900

    ColletcObjectの作成

--- CESA-2020-Prototype/Assets/Scripts/etc/Data.cs
+++ CESA-2020-Prototype/Assets/Scripts/etc/Data.cs
@@ -7,4 +7,5 @@ public class Data : MonoBehaviour
     public static int num_balloon = 0;
     public static int playerDir = 0;
     public static float playerVelX;
+    public static int colletcObject = 0;            // プレイ中のステージで獲得したCollectObjectの数
 }
