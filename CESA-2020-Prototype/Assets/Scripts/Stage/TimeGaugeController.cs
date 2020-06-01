770f8fb3feb1a014f61ad31327b86add74e6d8ae
onievui <onievui@gmali.com>
2020-06-01 19:07:28 +0900

    TimeGaugeがPlayerと重なる場合に半透明にするように

--- CESA-2020-Prototype/Assets/Scripts/Stage/TimeGaugeController.cs
+++ CESA-2020-Prototype/Assets/Scripts/Stage/TimeGaugeController.cs
@@ -15,15 +15,35 @@ public class TimeGaugeController : MonoBehaviour
     // member variable
     //------------------------------------------------------------------------------------------
     [SerializeField]
+    // テクスチャ
     private Image timeImage = null;
+    // キャンバスグループ
+    private CanvasGroup canvasGroup = null;
+    // プレイヤー
+    private GameObject player = null;
+
     [SerializeField]
+    // ゲージの割合のプロパティ名
     private string ratePropertyName = "";
 
-	//------------------------------------------------------------------------------------------
+    [SerializeField]
+    // プレイヤーと重なっているときのアルファ値
+    private float fadeAlpha = 0.2f;
+    [SerializeField]
+    // 透明になり始める距離
+    private float fadeStartRadius = 150.0f;
+    [SerializeField]
+    // 透明になり終わる距離
+    private float fadeEndRadius = 110.0f;
+
+
+
+    //------------------------------------------------------------------------------------------
     // Awake
-	//------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------
     private void Awake()
     {
+        canvasGroup = GetComponentInChildren<CanvasGroup>();
     }
 
 	//------------------------------------------------------------------------------------------
@@ -31,7 +51,7 @@ public class TimeGaugeController : MonoBehaviour
 	//------------------------------------------------------------------------------------------
     private void Start()
     {
-        
+        player = GameObject.Find(Player.NAME);
     }
 
 	//------------------------------------------------------------------------------------------
@@ -39,8 +59,24 @@ public class TimeGaugeController : MonoBehaviour
 	//------------------------------------------------------------------------------------------
 	private void Update()
     {
+        OverlapPlayer();
+
         Material mat = timeImage.material;
         mat.SetFloat(ratePropertyName, Data.time / Data.timeLimit);
+    }
+
+    //------------------------------------------------------------------------------------------
+    // プレイヤーとの重なり処理
+    //------------------------------------------------------------------------------------------
+    private void OverlapPlayer()
+    {
+        var playerPos = RectTransformUtility.WorldToScreenPoint(UnityEngine.Camera.main, player.transform.position);
+        var timePos = (Vector2)timeImage.rectTransform.position;
+
+        float dist = Vector2.Distance(playerPos, timePos);
+        
+        float t = (Mathf.Clamp(dist, fadeEndRadius, fadeStartRadius) - fadeEndRadius) / (fadeStartRadius - fadeEndRadius);
+        canvasGroup.alpha = Mathf.Lerp(fadeAlpha, 1.0f, t);
     }
 
     //------------------------------------------------------------------------------------------
