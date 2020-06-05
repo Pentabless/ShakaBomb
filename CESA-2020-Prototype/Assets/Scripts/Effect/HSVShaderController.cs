e63f40e850a6653ae8ec7d8dfef87e207f8d098f
onievui <onievui@gmali.com>
2020-06-05 16:17:36 +0900

    新しいバブルの画像を一部に適用 HSVShaderとそのコントローラーを実装

---
+++ CESA-2020-Prototype/Assets/Scripts/Effect/HSVShaderController.cs
@@ -0,0 +1,62 @@
+﻿//==============================================================================================
+/// File Name	: HSVShaderController.cs
+/// Summary		: HSVShaderの操作用スクリプト
+//==============================================================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Common;
+//==============================================================================================
+public class HSVShaderController : MonoBehaviour
+{
+    //------------------------------------------------------------------------------------------
+    // member variable
+    //------------------------------------------------------------------------------------------
+    private new Renderer renderer = null;  // レンダラー
+
+    [SerializeField]
+    [Header("色相を固定するかどうか")]
+    private bool useFixHue = false;
+    [SerializeField]
+    [Header("固定する色相")]
+    private Color fixHue = Color.red;
+    [SerializeField]
+    [Header("色相の変移")]
+    private float hueShift = 0;
+    [SerializeField]
+    [Header("彩度の倍率")]
+    private float saturationMultiply = 1;
+    [SerializeField]
+    [Header("彩度の変移")]
+    private float saturationShift = 0;
+    [SerializeField]
+    [Header("明度の倍率")]
+    private float brightnessMultiply = 1;
+    [SerializeField]
+    [Header("彩度の変移")]
+    private float brightnessShift = 0;
+
+	//------------------------------------------------------------------------------------------
+    // Awake
+	//------------------------------------------------------------------------------------------
+    private void Awake()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+	//------------------------------------------------------------------------------------------
+    // Update
+	//------------------------------------------------------------------------------------------
+	private void Update()
+    {
+        var mat = renderer.material;
+        mat.SetFloat("UseFixHue", (useFixHue ? 1 : 0));
+        mat.SetColor("FixHue", fixHue);
+        mat.SetFloat("HueShift", hueShift);
+        mat.SetFloat("SaturationMultiply", saturationMultiply);
+        mat.SetFloat("SaturationShift", saturationShift);
+        mat.SetFloat("BrightnessMultiply", brightnessMultiply);
+        mat.SetFloat("BrightnessShift", brightnessShift);
+    }
+}
---
+++ CESA-2020-Prototype/Assets/Shaders/HSVShader.shader
@@ -0,0 +1,87 @@
+﻿Shader "Unlit/HSVShader"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+		[MaterialToggle]
+		_UseFixHue("UseFixHue", Float) = 0
+		_FixHue("FixHue", Color) = (1, 1, 1, 1)
+		_HueShift("HueShift", Float) = 0
+		_SaturationMultiply("SaturationMultiply", Float) = 1
+		_SaturationShift("SaturationShift", Float) = 0
+		_BrightnessMultiply("BrightnessMultiply", Float) = 1
+		_BrightnessShift("BrightnessShift", Float) = 0
+		
+    }
+    SubShader
+    {
+		Tags { "RenderType" = "Transparent" "Queue" = "Transparent" }
+        LOD 100
+		Blend SrcAlpha OneMinusSrcAlpha
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+            #include "Common.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+				float4 color : COLOR;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+				float4 color : COLOR;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+			float _UseFixHue;
+			float4 _FixHue;
+			float _HueShift;
+			float _SaturationMultiply;
+			float _SaturationShift;
+			float _BrightnessMultiply;
+			float _BrightnessShift;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				o.color = v.color;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 diff = tex2D(_MainTex, i.uv);
+				float3 hsv = rgb2hsv(diff.rgb);
+
+				if (_UseFixHue > 0) {
+					hsv.x = frac(rgb2hsv(_FixHue.rgb).x + _HueShift);
+				}
+				else {
+					hsv.x = frac(hsv.x + _HueShift);
+				}
+
+				hsv.y = saturate(hsv.y * _SaturationMultiply + _SaturationShift);
+				hsv.z = saturate(hsv.z * _BrightnessMultiply + _BrightnessShift);
+
+				diff.rgb = hsv2rgb(hsv);
+				diff *= i.color;
+
+                return diff;
+            }
+            ENDCG
+        }
+    }
+}
