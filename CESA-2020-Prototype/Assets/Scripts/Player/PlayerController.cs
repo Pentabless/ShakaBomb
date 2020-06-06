53962a47fd63bb187b94f1199296245bf605f584
KubotaP <teapo6264@gmail.com>
2020-06-07 03:57:41 +0900

    ステージ制作途中経過 / タイルマップ更新

--- CESA-2020-Prototype/Assets/Scripts/Player/PlayerController.cs
+++ CESA-2020-Prototype/Assets/Scripts/Player/PlayerController.cs
@@ -574,12 +574,13 @@ public class PlayerController : MonoBehaviour
             isGround = true;
             jumpCount = 0;
             boostCount = 2;
-            // 着地エフェクト
-            Vector2 effectSize = Vector2.one * 0.4f;
-            EffectGenerator.BubbleBurstFX(
-                new BubbleBurstFX.Param(this.GetComponent<SpriteRenderer>().color, effectSize),
-                new Vector2(this.transform.position.x, this.transform.position.y - 1.0f),
-                null);
+
+            //// 着地エフェクト(保留)
+            //Vector2 effectSize = Vector2.one * 0.7f;
+            //EffectGenerator.BubbleBurstFX(
+            //    new BubbleBurstFX.Param(this.GetComponent<SpriteRenderer>().color, effectSize),
+            //    new Vector2(this.transform.position.x, this.transform.position.y - 1.0f),
+            //    null);
         }
 
         // バブルキャノン(仮)
