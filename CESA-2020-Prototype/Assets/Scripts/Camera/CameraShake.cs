3c7ccacb4fcd97a6732b90f85adad9a0b285ac27
onievui <onievui@gmali.com>
2020-06-13 11:53:55 +0900

    泡に包んだ敵に接触したときに、画面が揺れるように

--- CESA-2020-Prototype/Assets/Scripts/Camera/CameraShake.cs
+++ CESA-2020-Prototype/Assets/Scripts/Camera/CameraShake.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
+    // カメラの揺れている量
+    public Vector3 shakeMove { private set; get; } = Vector3.zero;
+    [SerializeField]
+    // カメラの揺れる間隔
+    private float shakeDelay = 1 / 60.0f;
+
     private IEnumerator DoShake(float duration, float magnitude)
     {
-        var pos = transform.localPosition;
-
-        var elapsed = 0f;
+        float elapsed = 0f;
 
         while (elapsed < duration)
         {
-            var x = pos.x + Random.Range(-1f, 1f) * magnitude;
-            var y = pos.y + Random.Range(-1f, 1f) * magnitude;
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
 
-            transform.localPosition = new Vector3(x, y, pos.z);
+            shakeMove = new Vector3(x, y, 0);
 
-            elapsed += Time.deltaTime;
+            elapsed += shakeDelay;
 
-            yield return null;
+            yield return new WaitForSeconds(shakeDelay);
         }
 
-        transform.localPosition = pos;
+        shakeMove = Vector3.zero;
     }
 
     public void Shake(float duration, float magnitude)
