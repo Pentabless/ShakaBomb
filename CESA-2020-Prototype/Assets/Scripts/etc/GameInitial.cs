c24f1f84b658cb435c53fd266244be6ca513669a
T.Yanase <TakumiYanase@outlook.com>
2020-06-14 17:48:26 +0900

    スクリーンの設定

--- CESA-2020-Prototype/Assets/Scripts/etc/GameInitial.cs
+++ CESA-2020-Prototype/Assets/Scripts/etc/GameInitial.cs
@@ -9,6 +9,6 @@ public class GameInitial
     [RuntimeInitializeOnLoadMethod]
     static void OnRuntimeMethodLoad()
     {
-        Screen.SetResolution(1920, 1080, false, 60);
+        Screen.SetResolution(1920, 1080, true);
     }
 }
